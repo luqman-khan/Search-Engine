07ca425d583651701ef458efdb6c118e0c275c82
DarrylCD <daredarryl@rediffmail.com>
2017-11-12 10:17:14 -0800

    calculating wdt

--- src/HashList.java
+++ src/HashList.java
@@ -6,120 +6,194 @@ import java.util.Set;
 public class HashList {
 
 	HashMap<String, ArrayList<Long>> posting_list;
-	int doc_id_pos, term_freq_pos;
-	
-	HashList(){
+	private int doc_id_pos, term_freq_pos;
+
+	HashList() {
 		posting_list = new HashMap<>();
 	}
-	
-	protected void add(Long doc_id, String term, long position ){
 
-		if(!this.posting_list.containsKey(term))
+	protected void add(Long doc_id, String term, long position) {
+
+		if (!this.posting_list.containsKey(term))
 			this.posting_list.put(term, new ArrayList<Long>());
 
 		ArrayList<Long> testList;
 		testList = this.posting_list.get(term);
-		if(testList.isEmpty()){
-			testList.add(new Long(2));//last doc id
+		if (testList.isEmpty()) {
+			testList.add(new Long(2));// last doc id
 			testList.add(new Long(1));// store doc frequence in second position
-			testList.add(doc_id);//doc id
+			testList.add(doc_id);// doc id
 			testList.add(new Long(0)); // term freq
-			testList.add(new Long(1)); // term rank
+			testList.add(new Long(1)); // term rank wdt
 		}
-		
-		
-//		if(this.posting_list.get(term).isEmpty()){
-//			this.posting_list.get(term).add(new Long(2));//last doc id
-//			this.posting_list.get(term).add(new Long(1));// store doc frequence in second position
-//			this.posting_list.get(term).add(doc_id);//doc id
-//			this.posting_list.get(term).add(new Long(0)); // term freq
-//			this.posting_list.get(term).add(new Long(1)); // term rank
-//		}
-		
-		
-		if(testList.get(testList.get(0).intValue()) != doc_id){
-			testList.set(1, testList.get(1)+1); // update doc freq
+
+		// if(this.posting_list.get(term).isEmpty()){
+		// this.posting_list.get(term).add(new Long(2));//last doc id
+		// this.posting_list.get(term).add(new Long(1));// store doc frequence
+		// in second position
+		// this.posting_list.get(term).add(doc_id);//doc id
+		// this.posting_list.get(term).add(new Long(0)); // term freq
+		// this.posting_list.get(term).add(new Long(1)); // term rank
+		// }
+
+		if (testList.get(testList.get(0).intValue()) != doc_id) {
+			testList.set(1, testList.get(1) + 1); // update doc freq
 			testList.add(doc_id); // add new doc id
-			testList.set(0, new Long(testList.size()-1));// update the position of last doc id
+			testList.set(0, new Long(testList.size() - 1));// update the
+															// position of last
+															// doc id
 			testList.add(new Long(0)); // term freq
 			testList.add(new Long(1)); // term rank
 		}
-		
-		
-		
-//		if(this.posting_list.get(term).get(this.posting_list.get(term).get(0).intValue()) != doc_id){
-//			this.posting_list.get(term).set(1, this.posting_list.get(term).get(1)+1); // update doc freq
-//			this.posting_list.get(term).add(doc_id); // add new doc id
-//			this.posting_list.get(term).set(0, new Long(this.posting_list.get(term).size()-1));// update the position of last doc id
-//			this.posting_list.get(term).add(new Long(0)); // term freq
-//			this.posting_list.get(term).add(new Long(1)); // term rank
-//		}
-		
+
+		// if(this.posting_list.get(term).get(this.posting_list.get(term).get(0).intValue())
+		// != doc_id){
+		// this.posting_list.get(term).set(1,
+		// this.posting_list.get(term).get(1)+1); // update doc freq
+		// this.posting_list.get(term).add(doc_id); // add new doc id
+		// this.posting_list.get(term).set(0, new
+		// Long(this.posting_list.get(term).size()-1));// update the position of
+		// last doc id
+		// this.posting_list.get(term).add(new Long(0)); // term freq
+		// this.posting_list.get(term).add(new Long(1)); // term rank
+		// }
+
 		testList.add(position);
-		int freq_pos = (testList.get(0)).intValue()+1;
-		testList.set(freq_pos, testList.get(freq_pos)+1 );
-		
-//		
-//		this.posting_list.get(term).add(position);
-//		int freq_pos = (this.posting_list.get(term).get(0)).intValue()+1;
-//		this.posting_list.get(term).set(freq_pos, this.posting_list.get(term).get(freq_pos)+1 );
+		int freq_pos = (testList.get(0)).intValue() + 1;
+		testList.set(freq_pos, testList.get(freq_pos) + 1);
+
+		//
+		// this.posting_list.get(term).add(position);
+		// int freq_pos = (this.posting_list.get(term).get(0)).intValue()+1;
+		// this.posting_list.get(term).set(freq_pos,
+		// this.posting_list.get(term).get(freq_pos)+1 );
 	}
-	
-	
-	Long[] getDocuments(String term){
+
+	Long[] getDocuments(String term) {
 		Long[] documents = new Long[0];
 		ArrayList<Long> testList;
 		testList = this.posting_list.get(term);
-//		System.out.println("inside getDocuments");
-		if(testList != null){
-//			System.out.println("inside test list not null");
-			long doc_frequency = testList.get(1); // get number of documents with that term
-			documents = new Long[(int)doc_frequency];
+		// System.out.println("inside getDocuments");
+		if (testList != null) {
+			// System.out.println("inside test list not null");
+			long doc_frequency = testList.get(1); // get number of documents
+													// with that term
+			documents = new Long[(int) doc_frequency];
 			int doc_id_pos = 2;
-			for(int i = 0 ;i<doc_frequency; i++){
-				documents[i]=testList.get(doc_id_pos);
-				doc_id_pos = doc_id_pos + testList.get(doc_id_pos+1).intValue() + 3;
+			for (int i = 0; i < doc_frequency; i++) {
+				documents[i] = testList.get(doc_id_pos);
+				doc_id_pos = doc_id_pos + testList.get(doc_id_pos + 1).intValue() + 3;
 			}
-//			System.out.println("done accumulationg documents");
-//			System.out.println("After done accumulationg documents 1 : length : "+documents.length + Arrays.asList(documents));
+			// System.out.println("done accumulationg documents");
+			// System.out.println("After done accumulationg documents 1 : length
+			// : "+documents.length + Arrays.asList(documents));
 		}
-//		System.out.println("After done accumulationg documents 2 : "+ Arrays.asList(documents));
+		// System.out.println("After done accumulationg documents 2 : "+
+		// Arrays.asList(documents));
 		return documents;
 	}
+
+	Long[] getTermFreq(String term) {
+		Long[] term_freq_array = new Long[0];
+		ArrayList<Long> testList;
+		testList = this.posting_list.get(term);
+		// System.out.println("inside getDocuments");
+		if (testList != null) {
+			// System.out.println("inside test list not null");
+			long doc_frequency = testList.get(1); // get number of documents
+													// with that term
+
+			term_freq_array = new Long[(int) doc_frequency*2];
+			int doc_id_pos = 2;
+			int term_freq_pos = 3;
+			System.out.println(testList);
+			for (int i = 0; i < term_freq_array.length; i++) {
+				term_freq_array[i] = testList.get(doc_id_pos);
+				i++;
+				term_freq_array[i] = testList.get(term_freq_pos);
+				doc_id_pos = doc_id_pos + testList.get(doc_id_pos + 1).intValue() + 3;
+				term_freq_pos = term_freq_pos + testList.get(term_freq_pos + 1).intValue() + 3;
+			}
+			//System.out.println("done accumulationg term frequencies of documents!!");
+			// System.out.println("After done accumulationg documents 1 : length
+			// : "+documents.length + Arrays.asList(documents));
+		}
+		// System.out.println("After done accumulationg documents 2 : "+
+		// Arrays.asList(documents));
+		return term_freq_array;
+	}
 	
-	Long [] getPostings(String term){
-		Long positions [];
-		ArrayList <Long> list_of_positions = new ArrayList<>();
+	Long[] setTermFreq(String term,Long [] a) {
+		Long[] term_freq_array = new Long[0];
+		Long[] only_term_freq_array = new Long[0];
+		ArrayList<Long> testList;
+		testList = this.posting_list.get(term);
+		// System.out.println("inside getDocuments");
+		if (testList != null) {
+//			 System.out.println("inside test list not null");
+//			 get number of documents with that term
+			long doc_frequency = testList.get(1); 
+											
+
+			term_freq_array = new Long[(int) doc_frequency*2];
+			only_term_freq_array = new Long[(int) doc_frequency];
+			int doc_id_pos = 2;
+			int term_freq_pos = 3;
+			System.out.println(testList);
+			for (int i = 0; i < term_freq_array.length; i++) {
+				term_freq_array[i] = testList.get(doc_id_pos);
+				i++;
+				term_freq_array[i] = testList.get(term_freq_pos);
+				doc_id_pos = doc_id_pos + testList.get(doc_id_pos + 1).intValue() + 3;
+				term_freq_pos = term_freq_pos + testList.get(term_freq_pos + 1).intValue() + 3;
+			}
+//			System.out.println("done accumulationg term frequencies of documents!!");
+//			 System.out.println("After done accumulationg documents 1 : length
+//			 : "+documents.length + Arrays.asList(documents));
+		}
+//		 System.out.println("After done accumulationg documents 2 : "+
+//		 Arrays.asList(documents));
+		return term_freq_array;
+	}
+
+	Long[] getPostings(String term) {
+		Long positions[];
+		ArrayList<Long> list_of_positions = new ArrayList<>();
 		ArrayList<Long> postings = this.posting_list.get(term);
-		int doc_id_skip_index = 5 + postings.get(3).intValue(); // get the index if next document id
-		
-		for(int i = 5; i<postings.size(); i++){
-			if(i==doc_id_skip_index){
-				doc_id_skip_index = i+postings.get(i+1).intValue()+3; //get next doc id position
-				i+=2;
+//		 get the index  if next document id
+		int doc_id_skip_index = 5 + postings.get(3).intValue(); 
+
+		for (int i = 5; i < postings.size(); i++) {
+			if (i == doc_id_skip_index) {
+				// get next doc id position
+				doc_id_skip_index = i + postings.get(i + 1).intValue() + 3; 
+				i += 2;
 				continue;
 			}
 			list_of_positions.add(postings.get(i));
-			 
+
 		}
 		positions = list_of_positions.toArray(new Long[list_of_positions.size()]);
 		return positions;
 	}
-	
-	Long [] getPositions(String term, Long document){
-		Long positions [] = new Long[0];
+
+	Long[] getPositions(String term, Long document) {
+		Long positions[] = new Long[0];
 		ArrayList<Long> postings = this.posting_list.get(term);
-		if(postings!=null){
-			int i=2; // position of first document ID
-			while(i<postings.size()){ //get the starting position of document ID
-				if(postings.get(i).equals(document))
+		if (postings != null) {
+//			 position of first document ID
+			int i = 2; 
+//			 get the starting position of document ID
+			while (i < postings.size()) { 
+				if (postings.get(i).equals(document))
 					break;
-				i = i+3+postings.get(i+1).intValue();
+				i = i + 3 + postings.get(i + 1).intValue();
 			}
-			if(i<postings.size()){
-				positions = new Long[postings.get(i+1).intValue()];
-				i = i + 3; // get index of first desired position
-				for(int j=0; j<positions.length; j++){
+			if (i < postings.size()) {
+				positions = new Long[postings.get(i + 1).intValue()];
+//				 get index of first desired position
+				i = i + 3; 
+				for (int j = 0; j < positions.length; j++) {
 					positions[j] = postings.get(i);
 					i++;
 				}
@@ -127,13 +201,13 @@ public class HashList {
 		}
 		return positions;
 	}
-	
-	 Set<String> keySet(){
+
+	Set<String> keySet() {
 		return this.posting_list.keySet();
 	}
 
 	public boolean containsKey(String term) {
-		if(this.posting_list.containsKey(term))
+		if (this.posting_list.containsKey(term))
 			return true;
 		else
 			return false;
--- src/InvertedIndex.java
+++ src/InvertedIndex.java
@@ -4,6 +4,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Scanner;
@@ -13,21 +14,23 @@ import javax.swing.JTextArea;
 
 public class InvertedIndex {
 
-//	protected Map<String, HashMap<Long, String>> pos_dictionary;
+	// protected Map<String, HashMap<Long, String>> pos_dictionary;
 	HashList pos_hash_list = new HashList();
 	protected HashMap<Long, String> files;
 	private long file_count = 0;
 	final Path folder_path;
+	private long total_docs = 0;
 
 	InvertedIndex(final Path directory) {
 		folder_path = directory;
 	}
-	
+
 	/**
-	 * traverse through all files in the directory and calls "buildDictionary" for each of the instance
+	 * traverse through all files in the directory and calls "buildDictionary"
+	 * for each of the instance
 	 */
 	public void indexDirectory() {
-//		pos_dictionary = new HashMap<String, HashMap<Long, String>>();
+		// pos_dictionary = new HashMap<String, HashMap<Long, String>>();
 		files = new HashMap<>();
 
 		try {
@@ -46,19 +49,86 @@ public class InvertedIndex {
 						files.put(file_count, file.getFileName().toString());
 						file_count++;
 					}
+					total_docs = file_count;
+
 					return FileVisitResult.CONTINUE;
 				}
 
 				public FileVisitResult visitFileFailed(Path file, IOException e) {
-
 					return FileVisitResult.CONTINUE;
 				}
+
 			});
-//			pos_hash_list.print();
+			// pos_hash_list.print();
+
 		} catch (Exception ex) {
 		}
 	}
-	
+
+	protected long[] calculateWdt(HashList pos_hash_list) {
+		String[] terms = getDictionary();
+		long a[] = null;
+		double wdt = 0;
+		 
+		for (int i = 0; i < terms.length; i++) {// for each term in corpus
+			System.out.println("term is  " + terms[i]);
+			Long[] term_freq_array = pos_hash_list.getTermFreq(terms[i]);
+			a = new long[term_freq_array.length / 2];
+			System.out.println("term freq array *******" + Arrays.asList(term_freq_array));
+			// for each tftd in term_freq_array
+			for (int j = 1; j < term_freq_array.length; j += 2) {
+//				double wdt = (1 + Math.log(term_freq_array[j])) * 100;
+//				multiply by 100 to save the two decimals after point
+				long wdtLong = (new Double((1 + Math.log(term_freq_array[j])) * 100)).longValue();
+				
+				for (int m = 0; m < a.length; m++) {
+					
+					
+					if (term_freq_array[j] > 0)
+						a[m] = wdtLong;
+					else
+						a[m] = 1;
+				}
+//				System.out.println("Wdt is ******* " + wdtLong);
+			}
+		}
+//		 pos_hash_list.getDocuments();
+		System.out.println("building long term freq array *******************");
+		 for(int l=0;l<a.length;l++){
+		 System.out.print("term array **************"+a[l]+",");}
+		return a;
+
+	}
+
+	// private double getTFTD(HashList pos_hash_list) {
+	// double tftd = 0;
+	// for(int i = 0; ;i++){
+	//
+	// }
+	// }
+
+	// private void calculateWqt(HashList pos_hash_list){
+	// String [] terms = getDictionary();
+	// for(int i =0;i<getDictionary().length;i++){
+	// //System.out.println(terms[i]);
+	// for(int j = 0;j<pos_hash_list.getDocuments(terms[i]).length;j++){
+	// double wdt = Math.log10(pos_hash_list.term_freq_pos);
+	// pos_hash_list.
+	// }
+	// }
+	// //pos_hash_list.getDocuments();
+	// }
+	// private void calculateLdt(HashList pos_hash_list){
+	// String [] terms = getDictionary();
+	// for(int i =0;i<getDictionary().length;i++){
+	// //System.out.println(terms[i]);
+	// for(int j = 0;j<pos_hash_list.getDocuments(terms[i]).length;j++){
+	// double wdt = Math.log10(pos_hash_list.term_freq_pos);
+	// pos_hash_list.
+	// }
+	// }
+	// //pos_hash_list.getDocuments();
+	// }
 	/**
 	 * builds inverted index of the file given in the argument
 	 */
@@ -69,21 +139,22 @@ public class InvertedIndex {
 				while (scan.hasNext()) {
 					String word = scan.next();
 					if (word.length() > 0) {
-						String [] word_array = word.trim().split("[-|@]");
-						for(String i : word_array){
-							if(!i.isEmpty() && !i.equals("[ ]+")){
+						String[] word_array = word.trim().split("[-|@]");
+						for (String i : word_array) {
+							if (!i.isEmpty() && !i.equals("[ ]+")) {
 								i = new Stemmer().processWord(i);
-								pos_hash_list.add(file_number,i,word_count);
+								pos_hash_list.add(file_number, i, word_count);
 								word_count++;
 							}
 						}
 					}
 				}
 			}
-		} catch (IOException ex) {}
+		} catch (IOException ex) {
+		}
 
 	}
-	
+
 	/**
 	 * outputs the vocab word list (stemmed)
 	 */
@@ -93,10 +164,11 @@ public class InvertedIndex {
 			vocab_string = vocab_string + "\n" + i.next();
 		return vocab_string;
 	}
+
 	public String[] getDictionary() {
 		// TODO Auto-generated method stub
 		Set<String> terms_set = this.pos_hash_list.keySet();
-		String [] terms = terms_set.toArray(new String [terms_set.size()]);
+		String[] terms = terms_set.toArray(new String[terms_set.size()]);
 		return terms;
 	}
 }
